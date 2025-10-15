3f2fb924cc2c1961cf852adaea171acea6a0eb52
xexxuoxe <dlwltn1283@naver.com>
2025-10-16 02:24:50 +0900

    env 수정

--- src/components/page/m/login/components/googleLoginButtons.component.tsx
+++ src/components/page/m/login/components/googleLoginButtons.component.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./loginButtons.module.scss";
 
 export default function GoogleLoginButtons() {
-  const link = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_API}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGL_REDIRECT_URL}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
+  const link = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_API}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
 
   const loginHandler = () => {
     window.location.href = link;
